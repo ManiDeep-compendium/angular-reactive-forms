51ff23e91e240fd0bd28d448a066fd92a8515e6f
gmanideep <gmanideep@qualminds.com>
2025-05-17 11:11:40 +0530

    Exploring around reactive forms: All an=bout reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormControl,ReactiveFormsModule, Validators} from '@angular/forms';
+import { CommonModule, JsonPipe } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule,JsonPipe,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'Reactive-Forms';
+  firstName: FormControl = new FormControl('',[Validators.required])
+  lastName: FormControl = new FormControl('',[Validators.required])
+  email: FormControl = new FormControl('',[Validators.required,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]);
+  passWord: FormControl = new FormControl('',[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)]);
+
+
+  onFormSubmit() {
+    console.log(this.firstName);
+    console.log(this.lastName);
+    console.log(this.email);
+  }
 }
